2a327c079486099d694ad466a772389ac01c1f19
Donovan <tsiotsio1409@gmail.com>
2026-09-17 21:37:15 -0600

    Initiaded instance piece and implement addPiece and getPiece methods.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -8,6 +8,8 @@ package chess;
  */
 public class ChessBoard {
 
+    private final ChessPiece[][] pieces = new ChessPiece[8][8];
+
     public ChessBoard() {
         
     }
@@ -19,7 +21,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        pieces[position.getRow() - 1][position.getColumn() - 1] = piece;
     }
 
     /**
@@ -30,7 +32,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return pieces[position.getRow() -1][position.getColumn() -1];
     }
 
     /**
